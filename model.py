1bcc88839ba4b5b8d5e838e3a1549abed8cdea34
Yipeng Jiang <yipengjiang0532@gmail.com>
2020-04-13 10:32:22 -0500

    some fix

--- model.py
+++ model.py
@@ -23,5 +23,21 @@ def model():
     predictions['predicted'] = lm2.predict(X)
     RMSE_lm2 = np.sqrt(mean_squared_error(predictions.actual, predictions.predicted))
     rfe_r2 = lm2.score(X, y)
-    print("RMSE", RMSE_lm2, "R2", rfe_r2)
+   
+    
+    #plot residuals
+    residuals = predictions.actual - predictions.predicted
+    plt.hlines(0, predictions.actual.min(), predictions.actual.max(), ls=':')
+    plt.scatter(predictions.actual, residuals)
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title('Actual vs Residual')
+    
+    #X_test = test.drop(columns = 'home_value')
+    y_test = test[['home_value']]
+    X_test_rfe = selector.transform(X_test)
+    y_test_predictions = lm2.predict(X_test_rfe)
+    RMSE_lm2_t = np.sqrt(mean_squared_error(y_test, y_test_predictions))
+    
+    print("RMSE", RMSE_lm2, "R2", rfe_r2, "Test_RMSE", RMSE_lm2_t)
     
--- split_scale.py
+++ split_scale.py
@@ -4,6 +4,10 @@ from prep import prep_data
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
+def split_data():
+    train, test = train_test_split(prep_data(), train_size = .8, random_state = 123)
+    return train, test
+
 def split_scale():
     train, test = train_test_split(prep_data(), train_size = .8, random_state = 123)
     scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(train)
